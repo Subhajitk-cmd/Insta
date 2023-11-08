398797c3ebbd0e71cf58ef28c218384ebed56115
Subhajitk-cmd <subhajitk706@gmail.com>
2023-11-09 00:38:37 +0530

    updated

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const mongoose = require('mongoose')  
 const {MONGOURI} = require('./keys')
-app.use(express.static('client/build'));
+
 if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'));
     const path = require('path')                                 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
